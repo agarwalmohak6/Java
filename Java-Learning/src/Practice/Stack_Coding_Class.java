a6ff83eca0bcd8425ac90290a4a24dcdc664fdfd
agarwalmohak6 <agarwalmohak6@gmail.com>
2022-02-16 11:50:56 +0530

    init

---
+++ Java-Learning/src/Practice/Stack_Coding_Class.java
@@ -0,0 +1,19 @@
+// Reverse words in a string using stack.
+
+package Practice;
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Stack<String> st=new Stack<>();
+        Scanner sc=new Scanner (System.in);
+        String s=sc.nextLine();
+        String[] arr =s.split("\\s+");
+        for(int i=0;i<arr.length;i++){
+            st.push(arr[i]);
+        }
+        while(!st.isEmpty())
+            System.out.print(st.pop()+" ");
+    }
+}
